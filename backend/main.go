0212f57c4633fdc7e1d173023fda540fd61137c5
Ashton Sobeck <asobeck@g.clemson.edu>
2023-08-19 22:59:47 -0400

    feat: get brew and get all brews; routing

--- backend/main.go
+++ backend/main.go
@@ -56,14 +56,17 @@ func main() {
 	s.handleRequests()
 }
 
-func (s *Server) makeNewToBrew(w http.ResponseWriter, r *http.Request) {
+func (s *Server) makeNewBrew(w http.ResponseWriter, r *http.Request) {
 	reqBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
 
 	var brew ToBrew
-	json.Unmarshal(reqBody, &brew)
+	err = json.Unmarshal(reqBody, &brew)
+	if err != nil {
+		panic(err)
+	}
 
 	if brew.Id == "" {
 		brew.Id = uuid.NewString()
@@ -78,7 +81,78 @@ func (s *Server) makeNewToBrew(w http.ResponseWriter, r *http.Request) {
 		brew.Id, brew.Name, brew.Bean, brewTime, time.Now().UTC())
 	tx.Commit()
 
-	json.NewEncoder(w).Encode(brew)
+	if err = json.NewEncoder(w).Encode(brew); err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) getBrew(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	if id != "" {
+		slog.Info("Getting Brew with Id: %s", id)
+
+		var brew ToBrew
+
+		// Get the brew and encode it back to the user
+		err := s.db.Get(&brew, `SELECT * FROM tobrews WHERE id=$1`, id)
+		if err != nil {
+			w.WriteHeader(500)
+			if _, err = w.Write([]byte(err.Error())); err != nil {
+				panic(err)
+			}
+		}
+
+		if err = json.NewEncoder(w).Encode(brew); err != nil {
+			panic(err)
+		}
+
+	}
+	// brew isn't found
+	w.WriteHeader(404)
+	if _, err := w.Write([]byte("Brew not found")); err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) getAllBrews(w http.ResponseWriter, r *http.Request) {
+	var brews []ToBrew
+
+	err := s.db.Select(&brews, "SELECT * FROM tobrews ORDER BY time_of_brew DESC")
+	if err != nil {
+		w.WriteHeader(500)
+		if _, err = w.Write([]byte(err.Error())); err != nil {
+			panic(err)
+		}
+	}
+
+	if len(brews) == 0 {
+		w.WriteHeader(404)
+		if _, err = w.Write([]byte("No brews found.")); err != nil {
+			panic(err)
+		}
+	}
+
+	if err = json.NewEncoder(w).Encode(brews); err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) brewRoutes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", s.getAllBrews)
+
+	r.Route("/{id}", func(r chi.Router) {
+		r.Get("/", s.getBrew)
+		r.Post("/", s.makeNewBrew)
+	})
+
+	return r
 }
 
 func (s *Server) handleRequests() {
@@ -91,7 +165,8 @@ func (s *Server) handleRequests() {
 		w.Write([]byte("hello world!"))
 	})
 
-	r.Post("/new-brew", s.makeNewToBrew)
+	r.Post("/new-brew", s.makeNewBrew)
+	r.Mount("/tobrews", s.brewRoutes())
 
 	http.ListenAndServe(":3333", r)
 }
