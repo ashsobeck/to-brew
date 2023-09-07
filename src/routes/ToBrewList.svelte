9433d822f1db0220846751b466970e6cbe1e2d58
Ashton Sobeck <asobeck99@gmail.com>
2023-09-07 06:49:59 -0400

    wip: brew list beautification

--- src/routes/ToBrewList.svelte
+++ src/routes/ToBrewList.svelte
@@ -9,18 +9,38 @@
 		console.log(b);
 		brews = b;
 	});
+
+	$: toBrew = brews.filter((b: ToBrew) => b.done === true);
+	$: brewed = brews.filter((b: ToBrew) => b.done === false);
 </script>
 
-<ul>
-	{#each brews as brew, i}
-		<div>
-			<li>{brew.bean}</li>
-			<li>{brew.name}</li>
-			<li>{brew.id}</li>
-			<li>{brew.time}</li>
-			<li>{brew.done}</li>
-			<li>{i}</li>
-		</div>
-	{/each}
-	<AddBrew />
-</ul>
+<div class="card p-4">
+	<ul>
+		{#each toBrew as brew, i}
+			<div class="card">
+				<li>{brew.bean}</li>
+				<li>{brew.name}</li>
+				<li>{brew.id}</li>
+				<li>{brew.time}</li>
+				<li>{brew.done}</li>
+				<li>{i}</li>
+			</div>
+		{/each}
+		<AddBrew />
+	</ul>
+</div>
+<div class="card p-4">
+	<ul>
+		{#each brewed as brew, i}
+			<div class="card">
+				<li>{brew.bean}</li>
+				<li>{brew.name}</li>
+				<li>{brew.id}</li>
+				<li>{brew.time}</li>
+				<li>{brew.done}</li>
+				<li>{i}</li>
+			</div>
+		{/each}
+		<AddBrew />
+	</ul>
+</div>
