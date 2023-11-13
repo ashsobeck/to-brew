b5b24f86e0dc90ea7ba0ea8da20936dcfa4ea26d
Ashton Sobeck <asobeck99@gmail.com>
2023-11-12 22:29:00 -0500

    refactor: simplify AddBrew

--- src/routes/AddBrew.svelte
+++ src/routes/AddBrew.svelte
@@ -5,17 +5,9 @@
 {#if showBrew}
 	<form method="POST" action="?/create">
 		<div class="card p-2">
-			<label class="label"
-				>Bean:
-				<select class="select" name="bean">
-					<option value="big bean"> bean bean </option>
-				</select>
-			</label>
+			<label class="label"><input class="input" name=""/></label>
 			<label class="label">Weight: <input class="input" name="weight" /> </label>
 			<label class="label">Time of Brew: <input class="input" name="time" /> </label>
-			<!-- <label class="label">Name: <input class="input" name="name" /> </label> -->
-			<!-- <label class="label">Roaster: <input class="input" name="roaster" /> </label> -->
-			<!-- <label class="label">Link: <input class="input" name="link" /> </label> -->
 			<button class="button" type="submit">Create</button>
 		</div>
 	</form>
