be9977e2e82a58e5c57fd10a4e1a3314ef05fb10
Ashton Sobeck <asobeck99@gmail.com>
2023-11-13 21:00:37 -0500

    very rough wip on beans route

--- src/routes/beans/+layout.svelte
+++ src/routes/beans/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-	import '../app.postcss';
+	import '../../app.postcss';
 </script>
 
-<slot />
+<div class="w-full h-screen grid grid-cols-[auto_1fr_auto] gap-x-1 items-center">
+	<div class="bg-surface-500/30 h-full card p-4">Beans <slot name="beans"/> </div>
+	<div class="bg-surface-500/30 h-full card p-4">current bean <slot name="current" /> </div>
+</div>
--- src/routes/beans/+page.svelte
+++ src/routes/beans/+page.svelte
@@ -1,1 +1,4 @@
-<div>Beans</div>
+<div class="w-full h-screen grid grid-cols-[auto_1fr_auto] gap-x-1 items-center">
+	<div class="bg-surface-500/30 h-full card p-4">Beans <slot name="beans"/> </div>
+	<div class="bg-surface-500/30 h-full card p-4">current bean <slot name="current" /> </div>
+</div>
